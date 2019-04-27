ba9d670afa616597783cb28ef97d08e746f84eb9
sb0002 <baneshellika@gmail.com>
2019-04-26 17:38:51 -0700

    got mnb done

--- classify.py
+++ classify.py
@@ -4,11 +4,11 @@ def readline(file0):
 	for line in lines:	
 		yield line.split()
 
-def tf():
+def tf(arg):
 	dictpos = {}
 	dictneg = {}
 	dictionary = {}
-	with open(datafile) as df, open('tf.csv', 'w+') as csvfile:
+	with open(arg) as df, open('tf.csv', 'w+') as csvfile:
 		lines = readline(df)
 		for line in lines:
 			words = line[1:]
@@ -19,7 +19,7 @@ def tf():
 						dictionary[word] = [1, 0]
 					else:
 						dictpos[word] += 1
-						dictionary[word][1] += 1
+						dictionary[word][0] += 1
 			if(line[0] == "-1"):
 				for word in words:
 					if word not in dictneg:
@@ -88,6 +88,7 @@ def tfgrep():
 		
 		mdw = allrows[row_with_biggest][0]
 		print("best word is %s with discrimination %d" % (mdw, maximum))
+
 	tp = 0
 	fp = 0
 	tn = 0
@@ -107,7 +108,8 @@ def tfgrep():
 				else:
 					fn += 1
 
-	print("confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+	print("tfgrep: confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
 
 	tp = 0
 	fp = 0
@@ -128,7 +130,8 @@ def tfgrep():
 				else:
 					fn += 1			#it's not in class 1 but we thought it was
 
-	print("confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+	print("tfgrep: confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
 	return mdw
 
 def priors():
@@ -158,27 +161,51 @@ def priors():
 
 	print("class 1 accounts for %f of the data, class -1 accounts for %f of the data." % (pos_pct, neg_pct))
 
-	print("confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
-	
-	return([pos_pct, neg_pct])
+	print("priors: confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+
+	tp = 0
+	tn = 0
+	fp = 0
+	fn = 0
+
+	with open(testfile) as tf:
+		lines = readline(tf)
+		for line in lines:
+			if(line[0] == "1"):
+				pos_ctr += 1
+				tp += 1
+			else:
+				neg_ctr += 1
+				fp += 1
+			total += 1
+
+	pos_pct = pos_ctr / total
+	neg_pct = neg_ctr / total
+
+	print("priors: confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+
+	return([pos_ctr, neg_ctr, total])
 
 def mnb():
 	temp = priors()
-	temp2 = tf()
 
-	prob_pos = temp[0]
-	prob_neg = temp[1]
-
-	dictionary = temp2
-
+	prob_pos = temp[0] / temp[2]
+	prob_neg = temp[1] / temp[2]
+	
 	total_pos = 0
 	total_neg = 0
+
+	tp = 0
+	tn = 0
+	fp = 0
+	fn = 0
 
 	cpost = 0
 	cnegt = 0
 
-	line_class_prediction = {}
-
+	dictionary = {}
 	with open('tf.csv', 'r') as csvfile:
 		rdr = csv.reader(csvfile, delimiter = ',')
 
@@ -186,38 +213,75 @@ def mnb():
 		for row in rdr:
 			cpost += int(row[1])
 			cnegt += int(row[2])
+			dictionary[str(row[0])] = [int(row[1]), int(row[2])]
 
-	print("debug: c1t: %d\tc-1t: %d" % (cpost, cnegt))
+	#print("debug: cpost: %d\tcnegt: %d" % (cpost, cnegt))
 
-	with open(datafile) as df, open('tf.csv', 'w+') as csvfile:
+	with open(datafile) as df:
 		lines = readline(df)
-		numerator = 0.0
-		demominator = 0.0
 		for line in lines:
-			line_dict = {}
-			for word in line:	#assemble a [word] : count dictionary for the line-doc
-				if(word == "1" or word == "-1"):
-					continue
-				if word in line_dict:
-					line_dict[word] += 1
+			linedict = {}
+			words = line[1:]			
+			sumclass1 = abs(math.log(prob_pos))
+			sumclass2 = abs(math.log(prob_neg))
+			
+			for word in words:						#for each line, make a dict out of its words
+				if(word in linedict):
+					linedict[word] += 1
 				else:
-					line_dict[word] = 1
+					linedict[word] = 1
 
+			for word in linedict:
+				sumclass1 += (math.log((1 + dictionary[word][0])/(1 + cpost)) * linedict[word])
+				sumclass2 += (math.log((1 + dictionary[word][1])/(1 + cnegt)) * linedict[word])
+
+			#print(str(sumclass1) + ":" + str(sumclass2))
+			if(sumclass1 >= sumclass2):
+				if(line[0] == "1"):
+					tp += 1
+				else:
+					fp += 1
+			else:
+				if(line[0] == "-1"):
+					tn += 1
+				else:
+					fn += 1
 		
-			for word in line_dict.keys():
-				#print(dictionary[word])
-				numerator += (line_dict[word] * math.log10((1 + dictionary[word][0])/(1 + cpost)))
-				demominator += (line_dict[word] * math.log10((1 + dictionary[word][1])/(1 + cnegt)))
-			result = (prob_pos * numerator)/(prob_neg * demominator)
-			#print("result = ", result)
-			if(result < 1):
-				pass
-				#predict class 1
-			if(result > 1):
-				#predict class -1
-				pass
-def df(arg):
-	pass
+		print("confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+		print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+
+def df():
+	class1w = {}
+	class2w = {}
+	allwords = {}
+	doc = {}
+
+	with open(datafile) as df:
+		lines = readline(df)
+		for line in lines:
+			doc = {}
+			for word in line[1:]:
+				allwords[word] = 1
+				if(line[0]) == "1":
+					doc[word] = "1"
+				else:
+					doc[word] = "-1"
+			for word in doc.keys():
+				if(doc[word] == "1"):
+					if word in class1w.keys():
+						class1w[word] += 1
+					else:
+						class1w[word] = 1
+				else:
+					if word in class2w.keys():
+						class2w[word] += 1
+					else:
+						class2w[word] = 1
+	
+	with open('df.csv', 'w+') as csvfile:
+		wtr = csv.writer(csvfile)
+		for word in allwords.keys():
+			wtr.writerow({"term":word,"class 1 frequency":class1w[word],"class -1 frequency":class2w[word]})
 
 def nb(arg):
 	pass
@@ -231,12 +295,14 @@ if __name__ == '__main__':
 	import csv
 	import ast
 	import math
+	import time
 
 	if(len(sys.argv) <= 3):
 		#print out help stuff
 		print("Usage:\nfunction\twhat it do\n-------------------------------------------\ntf\t\tcalculates term frequencies")
 		print("tfgrep\t\tcalculates most discriminating term")
-		#TODO: help message for all functions
+		print("priors\t\tcalculates majority class in each file")
+		print("mnb\t\tuses the multinomial naive bayes model to predict most likely class")
 		sys.exit(0)
 
 	datafile = sys.argv[1]
@@ -244,12 +310,14 @@ if __name__ == '__main__':
 	func = sys.argv[3]
 
 	if func == 'tf':
-		tf()
+		tf(datafile)
 	elif func == 'tfgrep':
 		tfgrep()
 	elif func == 'priors':
 		priors()
 	elif func == 'mnb':
 		mnb()
+	elif func == 'df':
+		df()
 	else:
 		sys.exit(0)
