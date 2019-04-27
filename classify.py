516e06f65f865292be53fe1a54ddef5b748cb0f0
sb0002 <baneshellika@gmail.com>
2019-04-26 23:00:41 -0700

    got df done

--- classify.py
+++ classify.py
@@ -4,7 +4,7 @@ def readline(file0):
 	for line in lines:	
 		yield line.split()
 
-def tf(arg):
+def tf(arg, print_flag):
 	dictpos = {}
 	dictneg = {}
 	dictionary = {}
@@ -36,7 +36,7 @@ def tf(arg):
 		second = list(dictneg.keys())
 		third = sorted(list(set(first + second)))
 
-		print('debug: total words: %d' % len(third))
+		#print('debug: total words: %d' % len(third))
 
 		dp = 0
 		dn = 0
@@ -51,14 +51,14 @@ def tf(arg):
 			else:
 				dn = dictneg[word]
 			wtr.writerow([word, str(dp), str(dn)])
+	if(print_flag):
+		print("Class 1:")
+		for pos in largestpos[:5]:
+			print(str(pos))
 
-	print("Class 1:")
-	for pos in largestpos[:5]:
-		print(str(pos))
-
-	print("Class -1:")
-	for neg in largestneg[:5]:
-		print(str(neg))
+		print("Class -1:")
+		for neg in largestneg[:5]:
+			print(str(neg))
 
 	return(dictionary)
 
@@ -110,7 +110,7 @@ def tfgrep():
 
 	print("tfgrep: confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
 	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
-
+	
 	tp = 0
 	fp = 0
 	tn = 0
@@ -132,9 +132,10 @@ def tfgrep():
 
 	print("tfgrep: confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
 	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+	
 	return mdw
 
-def priors():
+def priors(arg):
 	pos_ctr = 0
 	neg_ctr = 0
 	total = 0
@@ -158,11 +159,13 @@ def priors():
 
 	pos_pct = pos_ctr / total
 	neg_pct = neg_ctr / total
+	ret = [pos_pct, neg_pct]
 
-	print("class 1 accounts for %f of the data, class -1 accounts for %f of the data." % (pos_pct, neg_pct))
+	if(arg):
+		print("class 1 accounts for %f of the data, class -1 accounts for %f of the data." % (pos_pct, neg_pct))
 
-	print("priors: confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
-	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+		print("priors: confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+		print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
 
 	tp = 0
 	tn = 0
@@ -183,19 +186,17 @@ def priors():
 	pos_pct = pos_ctr / total
 	neg_pct = neg_ctr / total
 
-	print("priors: confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
-	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+	if(arg):
+		print("priors: confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+		print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
 
 	return([pos_ctr, neg_ctr, total])
 
 def mnb():
-	temp = priors()
+	temp = priors(False)
 
 	prob_pos = temp[0] / temp[2]
 	prob_neg = temp[1] / temp[2]
-	
-	total_pos = 0
-	total_neg = 0
 
 	tp = 0
 	tn = 0
@@ -204,7 +205,7 @@ def mnb():
 
 	cpost = 0
 	cnegt = 0
-
+	
 	dictionary = {}
 	with open('tf.csv', 'r') as csvfile:
 		rdr = csv.reader(csvfile, delimiter = ',')
@@ -224,18 +225,74 @@ def mnb():
 			words = line[1:]			
 			sumclass1 = abs(math.log(prob_pos))
 			sumclass2 = abs(math.log(prob_neg))
-			
-			for word in words:						#for each line, make a dict out of its words
+
+			for word in words:							#for each line, make a dict out of its words
 				if(word in linedict):
 					linedict[word] += 1
 				else:
 					linedict[word] = 1
 
 			for word in linedict:
-				sumclass1 += (math.log((1 + dictionary[word][0])/(1 + cpost)) * linedict[word])
-				sumclass2 += (math.log((1 + dictionary[word][1])/(1 + cnegt)) * linedict[word])
+					sumclass1 += (math.log((1 + dictionary[word][0])/(1 + cpost)) * linedict[word])
+					sumclass2 += (math.log((1 + dictionary[word][1])/(1 + cnegt)) * linedict[word])
 
-			#print(str(sumclass1) + ":" + str(sumclass2))
+			if(sumclass1 >= sumclass2):
+				if(line[0] == "1"):
+					tp += 1
+				else:
+					fp += 1
+			else:
+				if(line[0] == "-1"):
+					tn += 1
+				else:
+					fn += 1
+		
+		print("confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+		print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+	tf(testfile, False)
+	temp = priors(False)
+
+	prob_pos = temp[0] / temp[2]
+	prob_neg = temp[1] / temp[2]
+
+	tp = 0
+	tn = 0
+	fp = 0
+	fn = 0
+
+	cpost = 0
+	cnegt = 0
+	
+	dictionary = {}
+	with open('tf.csv', 'r') as csvfile:
+		rdr = csv.reader(csvfile, delimiter = ',')
+
+		#get total word count for each class
+		for row in rdr:
+			cpost += int(row[1])
+			cnegt += int(row[2])
+			dictionary[str(row[0])] = [int(row[1]), int(row[2])]
+
+	#print("debug: cpost: %d\tcnegt: %d" % (cpost, cnegt))
+
+	with open(testfile) as df:
+		lines = readline(df)
+		for line in lines:
+			linedict = {}
+			words = line[1:]			
+			sumclass1 = abs(math.log(prob_pos))
+			sumclass2 = abs(math.log(prob_neg))
+
+			for word in words:							#for each line, make a dict out of its words
+				if(word in linedict):
+					linedict[word] += 1
+				else:
+					linedict[word] = 1
+
+			for word in linedict:
+					sumclass1 += (math.log((1 + dictionary[word][0])/(1 + cpost)) * linedict[word])
+					sumclass2 += (math.log((1 + dictionary[word][1])/(1 + cnegt)) * linedict[word])
+
 			if(sumclass1 >= sumclass2):
 				if(line[0] == "1"):
 					tp += 1
@@ -249,42 +306,137 @@ def mnb():
 		
 		print("confusion matrix for testing data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
 		print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
+def df(arg):
+	with open(datafile) as df, open('df.csv', 'w+') as csvfile:
+		class1 = {}
+		class2 = {}
+		class_all = {}
+		lines = readline(df)
+		for line in lines:
+			words = line[1:]
+			uniq = []
+			if(line[0] == "1"):
+				for word in words:
+					if(word not in uniq):
+						uniq.append(word)
+						if(word not in class1):
+							class1[word] = 1
+							class_all[word] = [1, 0]
+						else:
+							class1[word] += 1
+							class_all[word][0] += 1
+			else:
+				for word in words:
+					if(word not in uniq):
+						uniq.append(word)
+						if(word not in class2):
+							class2[word] = 1
+							class_all[word] = [0, 1]
+						else:
+							class2[word] += 1
+							class_all[word][1] += 1
+		
+		largestpos = sorted(class1.items(), key = lambda item: item[1], reverse=True)
+		largestneg = sorted(class2.items(), key = lambda item: item[1], reverse=True)
 
-def df():
-	class1w = {}
-	class2w = {}
-	allwords = {}
-	doc = {}
+		words = set()
+		for word in class1:
+			words.add(word)
+		for word in class2:
+			words.add(word)
+
+		words = sorted(list(words))
+
+		wtr = csv.writer(csvfile)
+		wtr.writerow(["Term", "Class 1 Frequency", "Class -1 Frequency"])
+
+		for word in words:
+			to_write = [word]
+			if(word in class1):
+				to_write.append(class1[word])
+			else:
+				to_write.append(0)
+			if(word in class2):
+				to_write.append(class2[word])
+			else:
+				to_write.append(0)
+			wtr.writerow(to_write)
+		if(arg):
+			print("Class 1:")
+			for pos in largestpos[:5]:
+				print(str(pos))
+
+			print("Class -1:")
+			for neg in largestneg[:5]:
+				print(str(neg))
+		return(class_all)
+	
+def nb():
+	temp = priors(False)
+
+	prob_pos = temp[0] / temp[2]
+	prob_neg = temp[1] / temp[2]
+
+	dict1 = {}
+	dict2 = {}
+	
+	with open('df.csv') as dcsv:
+		rdr = csv.reader(dcsv, delimiter = ',')
+		for line in list(rdr)[1:]:
+			if(line[1]):
+				dict1[line[0]] = int(line[1])
+			if(line[2]):
+				dict2[line[0]] = int(line[2])
+
+	tp = 0
+	fp = 0
+	tn = 0
+	fn = 0
+
+	cpost = 0
+	cnegt = 0
+	
+	dictionary = {}
+	with open('tf.csv', 'r') as csvfile:
+		rdr = csv.reader(csvfile, delimiter = ',')
+
+		#get total word count for each class
+		for row in rdr:
+			cpost += int(row[1])
+			cnegt += int(row[2])
+			dictionary[str(row[0])] = [int(row[1]), int(row[2])]
 
 	with open(datafile) as df:
 		lines = readline(df)
 		for line in lines:
-			doc = {}
-			for word in line[1:]:
-				allwords[word] = 1
-				if(line[0]) == "1":
-					doc[word] = "1"
-				else:
-					doc[word] = "-1"
-			for word in doc.keys():
-				if(doc[word] == "1"):
-					if word in class1w.keys():
-						class1w[word] += 1
-					else:
-						class1w[word] = 1
-				else:
-					if word in class2w.keys():
-						class2w[word] += 1
-					else:
-						class2w[word] = 1
-	
-	with open('df.csv', 'w+') as csvfile:
-		wtr = csv.writer(csvfile)
-		for word in allwords.keys():
-			wtr.writerow({"term":word,"class 1 frequency":class1w[word],"class -1 frequency":class2w[word]})
+			linedict = {}
+			words = line[1:]			
+			sumclass1 = abs(math.log(prob_pos))
+			sumclass2 = abs(math.log(prob_neg))
 
-def nb(arg):
-	pass
+			for word in words:							#for each line, make a dict out of its words
+				if(word in linedict):
+					linedict[word] += 1
+				else:
+					linedict[word] = 1
+
+			for word in linedict:
+				sumclass1 += (math.log((1 + dict1[word])/(2 + cpost)))
+				sumclass2 += (math.log((1 + dict2[word])/(2 + cnegt)))
+
+			if(sumclass1 >= sumclass2):
+				if(line[0] == "1"):
+					tp += 1
+				else:
+					fp += 1
+			else:
+				if(line[0] == "-1"):
+					tn += 1
+				else:
+					fn += 1
+
+	print("confusion matrix for training data:\nTP  FN | %d  %d\nFP  TN | %d %d" % (tp, fn, fp, tn))
+	print("accuracy: " + str(100 * ((tp + tn)/(tp+fp+tn+fn))) + '%')
 
 def mine(arg):
 	pass
@@ -310,14 +462,16 @@ if __name__ == '__main__':
 	func = sys.argv[3]
 
 	if func == 'tf':
-		tf(datafile)
+		tf(datafile, True)
 	elif func == 'tfgrep':
 		tfgrep()
 	elif func == 'priors':
-		priors()
+		priors(True)
 	elif func == 'mnb':
 		mnb()
 	elif func == 'df':
-		df()
+		df(True)
+	elif func == 'nb':
+		nb()
 	else:
 		sys.exit(0)
